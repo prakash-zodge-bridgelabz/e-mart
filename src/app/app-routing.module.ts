00d620c9c9262d24170930ecfc353c16f6a7e627
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-01-12 23:27:27 +0530

    Before json connectivity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductsComponent } from './components/products/products.component';
 const routes: Routes = [
-  {path:'',redirectTo:'products',pathMatch:'full'},
-  {path:'products',component:ProductsComponent},
-  {path:'cart',component:CartComponent}
+  {
+    path:'',
+    redirectTo:'products',
+    pathMatch:'full'
+  },
+  {
+    path:'products',
+    component:ProductsComponent
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  }
 ];
 
 @NgModule({
